da82e2a734acc302083154c37d339bd6c3ee4a1f
Danny Tseng <dtfocus1025@gmail.com>
2019-04-07 00:35:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,10 @@ def oxford_comma(array)
   elsif array.size == 3
     array[2] = "and " + array[2]
     array.join(", ")
+    
+  else
+    array[array.size - 1] = "and " + array[array.size - 1]
+    array.join(", ")
   end
 
 end
